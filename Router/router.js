5ea0db69a040883a2239d9baaec3c171084937cd
AMRITHAASOK <amrithaasok.aa@gmail.com>
2025-01-08 18:10:30 +0530

    RestoCafe-angular-jun-24

--- Router/router.js
+++ Router/router.js
@@ -3,6 +3,7 @@ const express = require('express')
 const recipeController = require('../controllers/recipeController')
 const testimonyController=require('../controllers/testimonyController')
 const userController = require('../controllers/userController')
+const jwtMiddleware = require('../Middlewares/jwtMiddleware')
 const router = new express.Router()
 
 //get all recipes
@@ -12,4 +13,7 @@ router.post('/add-Testimony',testimonyController.addTestimony)
 //register
 router.post('/register',userController.register)
 router.post('/login',userController.login)
+
+//view recipee
+router.get('/viewRecipe/:id',jwtMiddleware,recipeController.viewRecipe)
 module.exports = router
